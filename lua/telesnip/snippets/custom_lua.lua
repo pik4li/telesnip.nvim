4ea0b11a13002493c443e4fbf17161e5e1ccfbe7
pika <alex@team-pieck.de>
2024-08-21 23:33:37 +0200

    removed some data

--- lua/telesnip/snippets/custom_lua.lua
+++
@@ -1,7 +0,0 @@
--- <name>
-testing
----
-M.save_custom_snippet = function()
-  local current_filetype = vim.bo.filetype
-  local custom_snippet_path = M.snippet_path .. "custom_" .. current_filetype .. ".lua"
-
