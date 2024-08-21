e228f58273f4c118e5263ec3d483ac561aa0648a
pika <alex@team-pieck.de>
2024-08-21 19:53:55 +0200

    big revamp 2.0?

--- lua/telesnip/snippets/lua/get_ip.lua
+++
@@ -1,4 +0,0 @@
-# ─< get the current ip as a 1 line >─────────────────────────────────────────────────────
-get_ip() {
-  command ip a | command grep 'inet ' | command grep -v '127.0.0.1' | command awk '{print $2}' | command cut -d/ -f1 | head -n 1
-}
---
+++ lua/telesnip/snippets/lua/snippets.lua
@@ -0,0 +1,10 @@
+greetings
+-- ─< simple lua function >─────────────────────────────────────────────────────────────
+local my_name = "pika"
+local function greetings(name)
+	print("Hello to", name)
+end
+
+greetings(my_name)
+
+---
